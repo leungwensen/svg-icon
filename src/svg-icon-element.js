85dc637c7edd11e0483ecd577f6138de4fc684e8
leungwensen <leungwensen@gmail.com>
2016-08-02 16:20:35 +0800

    add custom element for <svg-icon type="some"></svg-icon>

---
+++ src/svg-icon-element.js
@@ -0,0 +1,29 @@
+'use strict';
+/**
+ * svg-icon-element module
+ * @module svg-icon-element
+ * @see module:index
+ */
+const svgIcon = Object.create(HTMLElement.prototype);
+
+function generateSvgIcon(url, id) {
+  return `<svg class="si"><use xlink:href="${url}#${id}"></use></svg>`;
+}
+
+svgIcon._setContent = function () {
+  const url = this.getAttribute('url') || '';
+  const type = this.getAttribute('type');
+  this.innerHTML = generateSvgIcon(url, type);
+};
+
+svgIcon.createdCallback = function () {
+  this._setContent();
+};
+
+svgIcon.attributeChangedCallback = function () {
+  this._setContent();
+};
+
+document.registerElement('svg-icon', {
+  prototype: svgIcon
+});
