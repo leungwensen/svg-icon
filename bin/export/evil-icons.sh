fc5c20f2088a793f7ca6d7b8d8aeda42c4610629
leungwensen <leungwensen@gmail.com>
2017-02-04 18:02:48 +0800

    extracting evil-icons

---
+++ bin/export/evil-icons.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./bin/export-svg-icons.js -i ./dist/data/evil-icons.json -o ./temp/evil-icons.zip -p ei- --unzip
---
+++ bin/extract/evil-icons.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+const SVGO = require('svgo');
+const commander = require('commander');
+const fs = require('fs');
+const lang = require('zero-lang');
+const path = require('path');
+const request = require('request');
+const pkg = require('../../package.json');
+const extractSvgFiles = require('../../lib/extract-svg-files');
+const filterIcons = require('../../lib/filter-icons');
+const syncGitRepo = require('../sync-git-repo');
+
+const URL_GIT_REPO = 'git@github.com:evil-icons/evil-icons.git';
+const ICON_PATH = './assets/icons/';
+const DEFAULT_OUTPUT = path.resolve(__dirname, '../../dist/data/evil-icons.json');
+const DEFAULT_REPO_DIR = path.resolve(__dirname, '../../temp/.git-repo/evil-icons');
+
+function extractIcons(options) {
+  // less file for icon fonts
+  syncGitRepo(URL_GIT_REPO, options.dir, () => {
+    extractSvgFiles(path.join(options.dir, ICON_PATH), {}, (result) => {
+      lang.each(result, (icon) => {
+        icon.name = lang.trim(icon.name).replace(/^ei-/, '');
+      });
+      fs.writeFile(options.output, JSON.stringify(filterIcons(result), null, '\t'), (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`${commander.output} written!`);
+        }
+      });
+    });
+  });
+}
+
+commander
+  .version(pkg.version)
+  .option('-d, --dir [dir]', 'repo dir', DEFAULT_REPO_DIR)
+  .option('-o, --output [output]', 'output filename', DEFAULT_OUTPUT)
+  .parse(process.argv);
+
+extractIcons(commander);
