13c099453649293216ebe16d959829d44134a4be
leungwensen <leungwensen@gmail.com>
2017-02-04 18:39:49 +0800

    fixing zero icons

--- bin/extract/zero.js
+++ bin/extract/zero.js
@@ -2,12 +2,12 @@
 const commander = require('commander');
 const fs = require('fs');
 const path = require('path');
-const pkg = require('../package.json');
+const pkg = require('../../package.json');
 const extractSvgFiles = require('../../lib/extract-svg-files');
 const filterIcons = require('../../lib/filter-icons');
 
 const DEFAULT_OUTPUT = path.resolve(__dirname, '../../dist/data/zero.json');
-const DIRNAME = path.resolve(__dirname, '../src/zero');
+const DIRNAME = path.resolve(__dirname, '../../src/zero');
 
 commander
   .version(pkg.version)
