3e0af829d79022a3570c91c8853e033653d1ab83
leungwensen <leungwensen@gmail.com>
2017-02-04 11:23:35 +0800

    export icons in a right way

--- bin/export-svg-icons.js
+++ bin/export-svg-icons.js
@@ -2,6 +2,9 @@
 const commander = require('commander');
 const fs = require('fs');
 const path = require('path');
+const unzip = require('unzip2');
+const shelljs = require('shelljs');
+const fstream = require('fstream');
 const exportSvgIcons = require('../lib/export-svg-icons');
 const pkg = require('../package.json');
 
@@ -14,6 +17,7 @@ commander
   .option('-i, --input [input]', 'input icons filename', DEFAULT_INPUT)
   .option('-o, --output [output]', 'output filename', DEFAULT_OUTPUT)
   .option('-p, --prefix [prefix]', 'prefix', DEFAULT_PREFIX)
+  .option('--unzip', 'unzip icon pack')
   .parse(process.argv);
 
 const output = commander.output;
@@ -27,11 +31,24 @@ fs.readFile(input, 'utf8', (err, content) => {
     exportSvgIcons(icons, {
       prefix: commander.prefix,
     }, (zip) => {
+      // make sure we can write into the output file
+      shelljs.mkdir('-p', path.dirname(output));
       zip
         .generateNodeStream({type: 'nodebuffer', streamFiles: true})
         .pipe(fs.createWriteStream(output))
         .on('finish', () => {
           console.log(`${output} written.`)
+          if (commander.unzip) {
+            const unzipOuput = output.replace(/\.zip\s*$/, '');
+            // make sure the extracting path exist and empty
+            shelljs.rm('-rf', unzipOuput);
+            shelljs.mkdir('-p', unzipOuput);
+            const readStream = fs.createReadStream(output);
+            const writeStream = fstream.Writer(unzipOuput);
+            readStream
+              .pipe(unzip.Parse())
+              .pipe(writeStream);
+          }
         });
     });
   }
---
+++ bin/export/ant-design.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./bin/export-svg-icons.js -i ./dist/data/ant-design.json -o ./temp/ant-design.zip -p antd- --unzip
